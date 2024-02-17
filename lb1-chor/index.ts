7dae780b6838b121ed8f073c77053f81b5016d37
Gleb Andronchyk <gandronchyk@ek-mb-086.local>
2024-02-17 16:53:02 +0200

    refactor: lb1

--- lb1-chor/index.ts
+++ lb1-chor/index.ts
@@ -3,45 +3,59 @@ import { calculateStep } from "../global_settings";
 
 const result: number[] = [];
 
-const clarifyRootsByChord = (intervals: [number, number][]) => {
-  return intervals.map(([a, b]) => {
-    const step = calculateStep([a, b], dotsAmount);
+const getCalculationsArrays = (a: number, b: number, step: number) => {
+  const yArr = [fn(a)];
+  const xArr = Array.from({length: dotsAmount}).reduce<number[]>((acc, _, index) => {
+    if (index === 0) {
+      return acc;
+    }
 
-    const xArr = Array.from({length: dotsAmount}).reduce<number[]>((acc, _, index) => {
-      if (index === 0) {
+    if (index === dotsAmount - 1) {
+      yArr.push(fn(b));
+      return [...acc, b];
+    }
+
+    const x = acc[index - 1] + step;
+    yArr.push(fn(x));
+    return [...acc, x];
+  }, [a]);
+
+  return {yArr, xArr};
+}
+
+const getClarificationIntervals = (xArr: number[], yArr: number[]) => {
+  return yArr.reduce<[number, number][]>((acc, y, index) => {
+    if (yArr[index - 1] * y < 0) {
+      // When function changes sign => interval should be clarified by chord method
+      const x2 = chordFn(xArr[index - 1], xArr[index]);
+
+      // If function result is close enough => one solution found
+      if (Math.abs(fn(x2)) <= epsilon) {
+        result.push(x2);
         return acc;
       }
 
-      if (index === dotsAmount - 1) {
-        return [...acc, b];
-      }
+      return [
+        ...acc,
+        [xArr[index - 1], x2],
+        [x2, xArr[index]]
+      ];
+    }
 
-      return [...acc, acc[index - 1] + step];
-    }, [a]);
+    return acc;
+  }, []);
+}
 
+const clarifyRootsByChord = (intervals: [number, number][]) => {
+  intervals.forEach(([a, b]) => {
+    const step = calculateStep([a, b], dotsAmount);
 
-    const yArr = xArr.slice(0).reduce<number[]>((acc, y) => [...acc, fn(y)], []);
+    const {xArr, yArr} = getCalculationsArrays(a, b, step);
 
-    const intervalsForClarification = yArr.reduce<[number, number][]>((acc, y, index) => {
-      if (yArr[index - 1] * y < 0) {
-        const x2 = chordFn(xArr[index - 1], xArr[index]);
+    const intervalsForClarification = getClarificationIntervals(xArr, yArr);
 
-        if (Math.abs(fn(x2)) <= epsilon) {
-          result.push(x2);
-          return acc;
-        }
-
-        return [
-          ...acc,
-          [xArr[index - 1], x2],
-          [x2, xArr[index]]
-        ];
-      }
-
-      return acc;
-    }, []);
-
-    return clarifyRootsByChord(intervalsForClarification);
+    // Recursively find other solutions
+    intervalsForClarification.length && clarifyRootsByChord(intervalsForClarification);
   });
 }
 
