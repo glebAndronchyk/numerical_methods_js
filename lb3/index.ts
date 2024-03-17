190b22c7e56ccb07a573df206b725a27e958e3d3
Gleb Andronchyk <gleb.andronchyk@ekreative.com>
2024-03-17 18:12:41 +0200

    feat: newtons clarification logic (not full)

--- lb3/index.ts
+++ lb3/index.ts
@@ -1,13 +1,24 @@
+import {seidelClarify} from "../lb2-seidel";
+import {add} from "mathjs";
+import {Matrix} from "../lb2-seidel/types";
 
 type FnType = (x:number, y:number) => number;
 
+const epsilon = 0.01;
+
 class Equation {
+  private leftSide: FnType | null = null;
+
   constructor(private fn: FnType,
               private equals: number) {
   }
 
   toLeftSide(): FnType {
-    return (x: number, y: number) => this.fn(x, y) - this.equals;
+    if (!this.leftSide) {
+      this.leftSide = (x: number, y: number) => this.fn(x, y) - this.equals;
+    }
+
+    return this.leftSide;
   }
 
   intersects(eq: Equation): [number, number] {
@@ -19,7 +30,7 @@ class Equation {
     const dfx = (fn(x + d, y) - fn(x, y)) / d;
     const dfy = (fn(x, y + d) - fn(x, y)) / d;
 
-    return { dfx, dfy };
+    return [dfx, dfy];
   }
 }
 
@@ -27,32 +38,71 @@ class SONE {
 
   constructor(private equations: Equation[]) {}
 
-  solve = () => {
+  solve() {
     // const [x, y] = this.eq1.intersects(this.eq2);
-    const [x, y] = [0,0];
+    const [x, y] = [0.6,-0.4];
 
-    const [clarifiedX, clarifiedY] = this._clarifyStartValues(x, y);
+    const { x: clarifiedX, y: clarifiedY, jacobian } = this._clarifyStartValues(x, y);
+    // const P0 = [[clarifiedX], [clarifiedY]];
+
+    // This should be iteration
+    const B = this._getEquationsResultAtPoint(clarifiedX, clarifiedY);
+
+    // const deltas = seidelClarify(jacobian, B).map((d) => [d]);
+    // return this._newtonsClarify(P0, deltas);
   };
 
-  _clarifyStartValues(x: number, y: number) {
-    // LOOP maybe???
-    const derivation = this._createDerivativeMatrix(x, y);
-    if (!this._checkIntermediateResult(derivation)) {
-      //add step
-      return this._clarifyStartValues(x, y);
+
+  private _newtonsClarify(start: Matrix, deltas: Matrix) {
+    let prev = [...start];
+
+    let stop = 0;
+
+    while (true) {
+      const current = add(prev, deltas);
+
+      if (stop > 40) {
+        break;
+      }
+
+      // console.log(deltas);
+      // console.log(Math.abs(current[0][0] - prev[0][0]));
+      // console.log({cv: current[0][0], pv: prev[0][0]})
+
+      if (Math.abs(current[0][0] - prev[0][0]) <= epsilon) {
+        return current;
+      }
+
+      stop++;
+      prev = [...current];
     }
-
-    return [x, y];
-    // return [x, y, derivation]; ???????????? derivation === J
   };
-  _createDerivativeMatrix = (x, y) => {
-    return this.equations.map((eq) => {
-      const { dfx, dfy} = eq.getDerivativeByPoint(x, y);
+  private _getEquationsResultAtPoint(x, y) {
+    return this.equations.map((eq) => ([eq.toLeftSide()(x, y)]));
+  }
+  private _clarifyStartValues(x: number, y: number, step = 0.01) {
 
-      return [dfx, dfy];
-    })
+    let currX = x;
+    let currY = y;
+
+    const processCurrent = (val) => val >= 0 ? val - step : val + step;
+
+    while (true) {
+      const derivation = this._createDerivativeMatrixByPoint(currX, currY);
+
+      if (this._checkIntermediateResult(derivation)) {
+        return {x: currX, y: currY, jacobian: derivation};
+      }
+
+      currX = processCurrent(currX);
+      currY = processCurrent(currY);
+    }
   };
-  _checkIntermediateResult = (derivation: number[][], accuracy = 5) => {
+
+  private _createDerivativeMatrixByPoint = (x, y) => {
+    return this.equations.map((eq) => eq.getDerivativeByPoint(x, y));
+  };
+  private _checkIntermediateResult = (derivation: number[][], accuracy = 5) => {
     return derivation.reduce((acc,  [dfx, dfy]) => {
       if (Math.abs(dfx) + Math.abs(dfy) > accuracy) {
         acc = false;
@@ -64,7 +114,7 @@ class SONE {
 }
 
 
-const eq1 =  new Equation(() => 1, 21);
-const eq2 =  new Equation(() => 2, 22);
+const eq1 =  new Equation((x, y) => 2 * x - 4 * Math.pow(y, 3), 1.5);
+const eq2 =  new Equation((x, y) => 2 * Math.pow(x, 3) - 5 * Math.pow(y, 2), -0.25);
 const sone = new SONE([eq1, eq2]);
-sone.solve();
+console.log(sone.solve());
