fbeb1aa212286a4724cd66bdc2636c7ef54d8297
Gleb Andronchyk <gandronchyk@ek-mb-086.local>
2024-03-18 16:49:16 +0200

    fix: lb2 coefficients mutation

--- lb2-seidel/index.ts
+++ lb2-seidel/index.ts
@@ -2,18 +2,19 @@ import {epsilon, MAX_DISCREPANCY, INITIAL_COEFS, INITIAL_ITERATION_STATE, A, B} 
 import {Equation, Matrix} from "./types";
 import {getEquations, getNormalizedMatrix} from "./helpers";
 
-const iterate = (equations: Equation[], coefficients , state = {...INITIAL_ITERATION_STATE}): number[] | null => {
+const iterate = (equations: Equation[], coefficients: number[] , state = {...INITIAL_ITERATION_STATE}): number[] | null => {
 
   let accuracyCount = 0;
+  const coefficientsCopy = [...coefficients];
 
   equations.forEach((eq, index) => {
-    const coefsCopy =  [...coefficients];
+    const coefsCopy =  [...coefficientsCopy];
     coefsCopy[index] = null; // resets current coef in order to prevent it from taking part in calculations
 
     // Current X is being calculated
     const result = eq(...coefsCopy);
     // This is used in comparison with epsilon
-    const absoluteDifference = Math.abs(coefficients[index] - result);
+    const absoluteDifference = Math.abs(coefficientsCopy[index] - result);
     const prevAbsoluteDifference = state.prevAbsoluteDifferences[index];
     const isDifferencesNormal = !prevAbsoluteDifference || (prevAbsoluteDifference > absoluteDifference);
 
@@ -26,7 +27,7 @@ const iterate = (equations: Equation[], coefficients , state = {...INITIAL_ITERA
 
     // remembering current absolute difference
     state.prevAbsoluteDifferences[index] = absoluteDifference;
-    coefficients[index] = result;
+    coefficientsCopy[index] = result;
   });
 
   // when matrix diverged => I should normalize it
@@ -36,24 +37,24 @@ const iterate = (equations: Equation[], coefficients , state = {...INITIAL_ITERA
 
   // clarification ended
   if (accuracyCount === equations.length) {
-    return coefficients;
+    return coefficientsCopy;
   }
 
-  return iterate(equations, coefficients, state);
+  return iterate(equations, coefficientsCopy, state);
 }
 
 export const seidelClarify = (mA: Matrix, mB: Matrix) => {
   const equations = getEquations(mA, mB);
-  const coefficients = Array.from({length: mA[0].length}).fill(1) as number[];
+  const coefficientsCopy = Array.from({length: mA[0].length}).fill(1) as number[];
 
-  const firstIteration = iterate(equations, coefficients);
+  const firstIteration = iterate(equations, coefficientsCopy);
 
   if (!firstIteration) {
     // matrix diverged
     const {normalizedA, normalizedB} = getNormalizedMatrix(mA, mB);
     const normalizedEquations = getEquations(normalizedA, normalizedB)
 
-    return iterate(normalizedEquations, coefficients);
+    return iterate(normalizedEquations, coefficientsCopy);
   }
 
   return firstIteration;
